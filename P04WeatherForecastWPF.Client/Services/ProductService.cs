7aef840dd94707b17dadb1d1ba242a7e03e9179d
Tomasz Leś <szkolenia@tomaszles.pl>
2024-01-03 14:01:08 +0100

    q

---
+++ P04WeatherForecastWPF.Client/Services/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using P06Shop.Shared;
+using P06Shop.Shared.Services.ProductService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04WeatherForecastWPF.Client.Services
+{
+    internal class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ServiceReponse<List<Product>>> GetProductsAsync()
+        {
+            var response= await _httpClient.GetAsync(_httpClient.BaseAddress + "api/Product");
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ServiceReponse<List<Product>>>(json);
+            return result;
+        }
+    }
+}
